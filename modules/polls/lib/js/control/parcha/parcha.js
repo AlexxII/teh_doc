09418ea3e66f67a790ce59b203cbcdf84309bd0e
AlexxII <murmus51@yandex.ru>
2020-03-09 02:01:58 +0300

    -h riac update

--- modules/polls/lib/js/control/parcha/parcha.js
+++ modules/polls/lib/js/control/parcha/parcha.js
@@ -1,3 +1,5 @@
+let parchaTable, filteredArrayOfParchaSheeets, arrayOfParchaSheeets;
+
 function startParchaAnalyze() {
   let headerNode = document.getElementById('control-header');
   headerNode.innerHTML = '';
@@ -8,6 +10,28 @@ function startParchaAnalyze() {
 
   renderParchaHeader();
   renderParchaTbl()
+}
+
+// основной объект
+function parchaSheet(data, answers, answersRow, aObject) {
+  this.id = data.id.value;
+  this.user = data.usr_intrv.value;
+  this.date = data.date_intrv.value;
+  this.townType = '554 - город с численностью до 50 тыс.чел., поселок городского типа';
+  this.age = '507 - 50-59 лет';
+  this.gender = '501 - Женский';
+  this.startLt = data['start-lan'].value;
+  this.endLt = data['end-lat'].value;
+  this.startLn = data['start-lon'].value;
+  this.endLn = data['end-lon'].value;
+  this._status = +data.status.value;
+  this.answers = answers;
+  this.answersRow = answersRow;
+  this.aObject = aObject;                                             // ссылка на шит в исходном xml объекте
+
+  this.status = function () {
+    return this._status ? 'Принято' : 'Отклонен';
+  }
 }
 
 function renderParchaHeader() {
@@ -21,87 +45,30 @@ function renderParchaHeader() {
   mapBtn.id = 'parcha-show-map';
   mapBtn.addEventListener('click', mapsMe, false);
 
-  let unloadNode = document.createElement('a');
-  unloadNode.innerText = 'Выгрузить данные';
-  unloadNode.id = 'parcha-unload-data';
-  unloadNode.addEventListener('click', unloadData, false);
+  let unloadXmlNode = document.createElement('a');
+  unloadXmlNode.innerText = 'Выгрузить данные (XML)';
+  unloadXmlNode.id = 'parcha-unload-data';
+  unloadXmlNode.addEventListener('click', unloadData, false);
+
+  let unloadRawNode = document.createElement('a');
+  unloadRawNode.innerText = 'Выгрузить данные (Raw)';
+  unloadRawNode.id = 'parcha-unload-data';
+  unloadRawNode.addEventListener('click', renderRowAnswers, false);
 
   let btnsWrap = document.createElement('div');
   btnsWrap.className = 'parcha-btns';
 
   btnsWrap.appendChild(mapBtn);
-  btnsWrap.appendChild(unloadNode);
+  btnsWrap.appendChild(unloadXmlNode);
+  btnsWrap.appendChild(unloadRawNode);
 
   let hrNode = document.createElement('hr');
   wrapDiv.appendChild(xmlUploadTmpl());
-  wrapDiv.appendChild(blockOfSelectsTmpl());
+  wrapDiv.appendChild(blockOfSelects());
   wrapDiv.appendChild(hrNode);
   wrapDiv.appendChild(btnsWrap);
   headerNode.appendChild(wrapDiv);
-
 }
-
-var dataSet = [
-  ['parcha51-3', '10.03.2020', '554 - город с численностью до 50 тыс.чел. пгт', '501 - Женский', '507 - 50-59 лет', 'Принято'],
-  ['parcha51-4', '09.03.2020', '554 - город с численностью до 50 тыс.чел. пгт', '502 - Мужской', '506 - 30-49 лет', 'Принято']
-];
-
-var dataSetEx = [];
-
-
-function renderTbl() {
-  let wrapDiv = document.createElement('div');
-  wrapDiv.id = 'parcha-wrap';
-  let tableNode = document.createElement('table');
-  tableNode.id = 'parcha-table';
-  tableNode.className = 'display';
-  tableNode.width = '100%';
-  wrapDiv.appendChild(tableNode);
-  return wrapDiv;
-}
-
-function renderParchaTbl() {
-  let resultNode = document.getElementById('control-result');
-  resultNode.appendChild(renderTbl());
-
-  $('#parcha-table').DataTable({
-    data: dataSet,
-    responsive: true,
-    searching: false,
-    columns: [
-      {title: 'Планшет'},
-      {title: 'Дата'},
-      {title: 'Тип населенного пункта'},
-      {title: 'Пол'},
-      {title: 'Возраст'},
-      {title: 'Статус'},
-      {},
-      {}
-    ],
-    columnDefs: [
-      {
-        'targets': -2,                    // предпоследний столбец
-        'orderable': false,
-        'data': null,
-        'width': '70px',
-        'defaultContent': 'Просмотр/Карта'
-      }, {
-        'targets': -1,                    // последний столбец
-        'orderable': false,
-        'className': 'select-checkbox',
-        'defaultContent': ''
-      }
-    ],
-    select: {
-      style: 'multi',
-      selector: 'td:last-child',
-    },
-    language: {
-      url: '/lib/ru.json'
-    }
-  });
-}
-
 
 function xmlUploadTmpl() {
   let divForm = document.createElement('div');
@@ -116,26 +83,31 @@ function xmlUploadTmpl() {
   inputNode.id = 'parcha-upload';
   inputNode.type = 'file';
   divForm.appendChild(inputNode);
-  inputNode.addEventListener('change', loadAndParseXmlFile, false);
+  inputNode.addEventListener('change', loadXmlFile, false);
   return divForm;
 }
 
-function unloadData() {
-
-}
-
-function blockOfSelectsTmpl() {
+function blockOfSelects() {
   let selectBlock = document.createElement('div');
   selectBlock.className = 'parcha-select-block';
 
   let operatorSelect = document.createElement('select');
   let operatorLabel = document.createElement('label');
-  operatorLabel.innerText = 'Планшеты:';
+  operatorLabel.innerText = 'Планшет:';
   operatorLabel.className = 'parcha-select-labels';
   operatorSelect.id = 'parcha-operators';
   operatorSelect.classList = 'form-control';
   operatorSelect.disabled = true;
   operatorLabel.appendChild(operatorSelect);
+
+  let dateSelect = document.createElement('select');
+  let dateLabel = document.createElement('label');
+  dateLabel.innerText = 'Датa:';
+  dateLabel.className = 'parcha-select-labels';
+  dateSelect.id = 'parcha-date';
+  dateSelect.classList = 'form-control';
+  dateSelect.disabled = true;
+  dateLabel.appendChild(dateSelect);
 
   let townSelect = document.createElement('select');
   let townLabel = document.createElement('label');
@@ -174,6 +146,7 @@ function blockOfSelectsTmpl() {
   statusLabel.appendChild(statusSelect);
 
   selectBlock.appendChild(operatorLabel);
+  selectBlock.appendChild(dateLabel);
   selectBlock.appendChild(townLabel);
   selectBlock.appendChild(sexLabel);
   selectBlock.appendChild(ageLabel);
@@ -183,81 +156,160 @@ function blockOfSelectsTmpl() {
 
 }
 
-function loadAndParseXmlFile() {
+var dataSet = [
+  ['parcha51-3', '10.03.2020', '554 - город с численностью до 50 тыс.чел. пгт', '501 - Женский', '507 - 50-59 лет', 'Принято'],
+  ['parcha51-4', '09.03.2020', '554 - город с численностью до 50 тыс.чел. пгт', '502 - Мужской', '506 - 30-49 лет', 'Принято']
+];
+
+var dataSetEx = [];
+
+function renderTbl() {
+  let wrapDiv = document.createElement('div');
+  wrapDiv.id = 'parcha-wrap';
+  let tableNode = document.createElement('table');
+  tableNode.id = 'parcha-table';
+  tableNode.className = 'display';
+  tableNode.width = '100%';
+  wrapDiv.appendChild(tableNode);
+  return wrapDiv;
+}
+
+function renderParchaTbl() {
+  let resultNode = document.getElementById('control-result');
+  resultNode.appendChild(renderTbl());
+
+  parchaTable = $('#parcha-table').DataTable({
+    data: filteredArrayOfParchaSheeets,
+    responsive: true,
+    searching: false,
+    columns: [
+      {title: 'id', data: 'id'},
+      {title: 'Планшет', data: 'user'},
+      {title: 'Дата', data: 'date'},
+      {title: 'Тип населенного пункта', data: 'townType'},
+      {title: 'Пол', data: 'gender'},
+      {title: 'Возраст', data: 'age'},
+      {title: 'Статус', data: 'status'},
+      {},
+      {}
+    ],
+    columnDefs: [
+      {
+        'targets': -2,                    // предпоследний столбец
+        'orderable': false,
+        'data': null,
+        'width': '70px',
+        'defaultContent': 'Просмотр/Карта'
+      }, {
+        'targets': -1,                    // последний столбец
+        'orderable': false,
+        'className': 'select-checkbox',
+        'defaultContent': ''
+      }
+    ],
+    select: {
+      style: 'multi',
+      selector: 'td:last-child',
+    },
+    language: {
+      url: '/lib/ru.json'
+    }
+  });
+}
+
+// функция обработки события добавления xml
+function loadXmlFile() {
   let xmlFile = this.files[0];
   let reader = new FileReader();
   reader.onload = function (e) {
     let result = reader.result;
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(result, "text/xml");
-    let poll = doc.getElementsByTagName('opros')[0];
-    let sheets = poll.children;
-    let length = sheets.length;
-
-    let temp = [];
-    let tempEx = {};
-
-    for (let i = 0; i < length; i++) {
-      let data = sheets[i].attributes;
-      temp[i] = new pollSheet(data);
-
-      let sheetId = sheets[i].attributes.id.value;
-      tempEx[sheetId] = sheets[i];
-
-    }
-    sheeets = temp;
-    sheeetObjs = tempEx;
-
-    let headerNode = document.getElementById('analytic-header');
-    let mapBtn = document.createElement('a');
-    mapBtn.id = 'maps-me';
-    mapBtn.innerText = 'На карте';
-    headerNode.appendChild(mapBtn);
-    mapBtn.addEventListener('click', mapsMe, false);
-
-    let t = new XMLSerializer();
-    let tt = t.serializeToString(doc);
-    console.log(tt);
-
+    parseLoadedXml(result);
   };
   reader.readAsText(xmlFile);
 }
 
-function pollSheet(data) {
-  this.id = data.id.value;
-  this.user = data.usr_intrv.value;
-  this.date = data.date_intrv.value;
-  this.startLt = data['start-lan'].value;
-  this.endLt = data['end-lat'].value;
-  this.startLn = data['start-lon'].value;
-  this.endLn = data['end-lon'].value;
+function parseLoadedXml(result) {
+  let
+    doc = tryParseXML(result);
+  if (doc) {
+    let poll = doc.getElementsByTagName('opros')[0];
+    let sheets = poll.children;                                             // коллекция Шитов
+    let length = sheets.length;
+    let temp = [];
+    for (let i = 0; i < length; i++) {                                      // xml объект Шита
+      let sheetId = sheets[i].attributes.id.value;
+      let data = sheets[i].attributes;
+      let questions = sheets[i].children;
+      let length = questions.length;
+      let poolOfQuestions = {}, answersRow = [];
+      for (let i = 0; i < length; i++) {
+        let question = questions[i];
+        let qNumber = question.attributes[0].value;
+        let answers = question.children;
+        let aLength = answers.length;
+        let poolOfAnswers = [];
+        for (let j = 0; j < aLength; j++) {
+          let answerCode = answers[j].attributes[0].value;
+          poolOfAnswers[j] = answerCode;
+          answersRow.push(answerCode);
+        }
+        poolOfQuestions[qNumber] = poolOfAnswers;
+      }
+      temp[i] = new parchaSheet(data, poolOfQuestions, answersRow, sheets[i]);
+    }
+    console.log(temp);
+    arrayOfParchaSheeets = temp;
+    filteredArrayOfParchaSheeets = temp;
+
+    // let headerNode = document.getElementById('analytic-header');
+    // let mapBtn = document.createElement('a');
+    // mapBtn.id = 'maps-me';
+    // mapBtn.innerText = 'На карте';
+    // headerNode.appendChild(mapBtn);
+    // mapBtn.addEventListener('click', mapsMe, false);
+
+    // let t = new XMLSerializer();
+    // let tt = t.serializeToString(doc);
+    // console.log(tt);
+    // renderParchaTbl();
+
+    $('#parcha-table').DataTable().data.reload();
+
+    // $('#parcha-table').data.reload();
+
+    // console.log(parchaTable);
+    // parchaTable.reload();
+
+    return;
+  }
+  let errorText = '<span style="font-weight: 600">Что-то пошло не так!</span><br>Ошибка при анализе XML. См. консоль';
+  initNoty(errorText, 'warning');
+
+}
+
+// функия проверки корректности XML
+function tryParseXML(xmlString) {
+  var parser = new DOMParser();
+  var parsererrorNS = parser.parseFromString('INVALID', 'text/xml').getElementsByTagName("parsererror")[0].namespaceURI;
+  var dom = parser.parseFromString(xmlString, 'text/xml');
+  if (dom.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0) {
+    console.log(dom.childNodes[1].innerHTML);
+    return false;
+  }
+  return dom;
+}
+
+function initFilterSelects() {
+
 }
 
 function parseSelectedMarkers(markers) {
   let detailData = {};
-  markers.forEach(function (marker, index) {
-    // let needQuestions = [23, 24, 25, 26, 27, 28];
-    let id = marker.options.id;
-    let sheet = sheeetObjs[id];
-    let questions = sheet.children;
-    let length = questions.length;
-
-    let data = sheet.attributes;
-
-    let pollOfAnswers = [];
-    for (let i = 0; i < length; i++) {
-      let question = questions[i];
-      let qNumber = question.attributes[0].value;
-      // if (needQuestions.includes(+qNumber)) {
-      let answer = question.children[0].attributes[0].value;
-      pollOfAnswers.push(answer);
-      // }
-    }
-    detailData[data.id.value] = new detailSheet(data, pollOfAnswers);
-  });
   renderParchaResults(detailData);
-
 }
+
+// let needQuestions = [23, 24, 25, 26, 27, 28];
+// if (needQuestions.includes(+qNumber)) {
 
 function mapsMe() {
   let selectedMarkers, childCount;
@@ -293,21 +345,23 @@ function mapsMe() {
         self.buttons.ok.enable();
       });
 
-      sheeets.forEach(function (sheet, i) {
-        let marker = L.marker([sheet.endLt, sheet.endLn], {
-          id: sheet.id,
-          title: sheet.user
+      if (filteredArrayOfParchaSheeets) {
+        filteredArrayOfParchaSheeets.forEach(function (sheet, i) {
+          let marker = L.marker([sheet.endLt, sheet.endLn], {
+            id: sheet.id,
+            title: sheet.user
+          }).on('click', markerClick);
+          marker.bindPopup(
+            '<strong>ID: </strong>' + sheet.id + '<br>' +
+            '<strong>Планшет: </strong>' + sheet.user + '<br>' +
+            '<strong>Дата: </strong>' + sheet.date + '<br>' +
+            '<strong> Координаты: </strong>' + sheet.endLt + ' с.ш. | ' + sheet.endLn + ' в.д.' + '<br>'
+          );
+          m.addLayer(marker);
+          // marker.addTo(map);
         });
-        marker.bindPopup(
-          '<strong>' + sheet.user + '</strong> ' +
-          '<hr>' +
-          '<strong>Дата: </strong>' + sheet.date + '<br>' +
-          '<strong> Координаты: </strong>' + sheet.endLt + ' с.ш. | ' + sheet.endLn + ' в.д.' + '<br>'
-        );
-        m.addLayer(marker);
-        // marker.addTo(map);
-      });
-      map.addLayer(m);
+        map.addLayer(m);
+      }
     },
     buttons: {
       ok: {
@@ -315,7 +369,7 @@ function mapsMe() {
         text: 'Выбрать',
         action: function () {
           jc.close();
-          parseSelectedMarkers(selectedMarkers);
+          filterSelectedMarkers(selectedMarkers);
         }
       },
       cancel: {
@@ -326,33 +380,60 @@ function mapsMe() {
   });
 }
 
-function detailSheet(data, answers) {
-  this.id = data.id.value;
-  this.user = data.usr_intrv.value;
-  this.date = data.date_intrv.value;
-  this.startLt = data['start-lan'].value;
-  this.endLt = data['end-lat'].value;
-  this.startLn = data['start-lon'].value;
-  this.endLn = data['end-lon'].value;
-  this.answers = answers;
+function markerClick(e) {
+  console.log(e);
 }
 
-function renderParchaResults(data) {
-  console.log(data);
-  let footerNode = document.getElementById('control-footer');
-  footerNode.innerHTML = '';
-  let hrNode = document.createElement('hr');
-  footerNode.appendChild(hrNode);
+function filterSelectedMarkers(markers) {
+  markers.forEach(function (marker, index) {
+    let id = marker.options.id;
+    let sheet = sheeetObjs[id];
+    let questions = sheet.children;
+    let length = questions.length;
+    let data = sheet.attributes;
+    let pollOfAnswers = [];
+    for (let i = 0; i < length; i++) {
+      let question = questions[i];
+      let qNumber = question.attributes[0].value;
+      let answer = question.children[0].attributes[0].value;
+      pollOfAnswers.push(answer);
+    }
+  });
+}
+
+function renderRowAnswers() {
+  let jc = $.confirm({
+    title: 'Raw данные',
+    columnClass: 'xlarge',
+    content: '<div id="parcha-raw-data"></div>',
+    onContentReady: function () {
+      let self = this;
+
+
+    },
+    buttons: {
+      cancel: {
+        text: 'НАЗАД'
+      }
+    }
+
+  });
+
+
+
+
   let divNode = document.createElement('div');
-  footerNode.appendChild(divNode);
-  for (let key in data) {
+  for (let key in sheets) {
     let spanNode = document.createElement('span');
     let brNode = document.createElement('br');
-    // spanNode.innerText = data[key].user + ' - ' + data[key].answers.toString();
-    spanNode.innerText = data[key].answers.toString();
+    // spanNode.innerText = sheets[key].user + ' - ' + sheets[key].answers.toString();
+    spanNode.innerText = sheets[key].answers.toString();
     divNode.appendChild(spanNode);
     divNode.appendChild(brNode);
   }
+}
+
+function unloadData() {
 
 }
 
